a30f5114430d236c62be6469fe7a45da30a17b9a
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-05-17 22:38:42 +0300

    completed

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,37 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+        print("Happy New Year")
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    square_num = [num**2 for num in int_list]
+    return square_num
 
-def fizzbuzz():
+
+def fizzbuzz(num):
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if num % 3 == 0 & num % 5 == 0:
+            return "FizzBuzz"
+        elif num % 3 == 0:
+            return "Fizz"
+        elif num % 5 == 0:
+            return "Buzz"
+        else:
+            return num
+
+
+# # i=0
+# # while i<5:
+# #     print("Looping")
+# #     i+=1
+
+# for i in range(10):
+#     print("looping")
+#     print(f"i is:{i}")
